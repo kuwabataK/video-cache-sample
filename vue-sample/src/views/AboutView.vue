b11b81b63c17dc6ca87df40f238aeaee7b5318ef
KuwabataK <monorod98@gmail.com>
2024-08-09 19:01:57 +0900

    videoファイルのロード処理を追加

--- vue-sample/src/views/AboutView.vue
+++ vue-sample/src/views/AboutView.vue
@@ -1,8 +1,45 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <video controls type="video/mp4">
+    </video>
+    <button @click="loadVideo">Load Video</button>
+    <button @click="cacheVideo">Cache Video</button>
   </div>
 </template>
+
+<script setup lang="ts">
+import { onUnmounted } from 'vue'
+
+const videoURL = '/sample-video.mp4'
+
+const cacheVideo = async () => {
+  const cache = await caches.open('video-cache')
+  return cache.add(videoURL)
+}
+
+const loadVideo = async () => {
+  const cache = await caches.open('video-cache')
+  const response = await cache.match(videoURL)
+  if (response) {
+    const blob = await response.blob()
+    const video = document.querySelector('video')
+    if (video) {
+      video.src = URL.createObjectURL(blob)
+      video.controls = true
+    }
+  }
+}
+
+onUnmounted(() => {
+  const video = document.querySelector('video')
+  if (video) {
+    video.pause()
+    video.remove()
+    URL.revokeObjectURL(video.src)
+  }
+})
+
+</script>
 
 <style>
 @media (min-width: 1024px) {
