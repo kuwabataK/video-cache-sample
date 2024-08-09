9a6804ff0bd31ce7ea6e5eabe84e66e39c49065f
KuwabataK <monorod98@gmail.com>
2024-08-09 20:44:50 +0900

    リファクタリング

--- vue-sample/src/views/AboutView.vue
+++ vue-sample/src/views/AboutView.vue
@@ -1,44 +1,49 @@
 <template>
   <div class="about">
-    <video controls type="video/mp4">
-    </video>
+    <video controls type="video/mp4" :src="videoObjectURL"></video>
     <button @click="loadVideo">Load Video</button>
     <button @click="cacheVideo">Cache Video</button>
+    <button @click="deleteVideo">Delete Video</button>
   </div>
 </template>
 
 <script setup lang="ts">
-import { onUnmounted } from 'vue'
+import { onUnmounted, ref } from 'vue'
 
 const videoURL = '/sample-video.mp4'
 
+const videoObjectURL = ref('')
+
 const cacheVideo = async () => {
+  // キャッシュストレージの名前を指定してキャッシュインスタンスを生成
   const cache = await caches.open('video-cache')
+  // キャッシュストレージにリソースを追加
   return cache.add(videoURL)
 }
 
 const loadVideo = async () => {
+  // キャッシュストレージの名前を指定してキャッシュインスタンスを生成
   const cache = await caches.open('video-cache')
+  // キャッシュストレージからリソースを取得
   const response = await cache.match(videoURL)
   if (response) {
+    // キャッシュが合った場合は、そのレスポンスをblobにしてvideo要素のsrcに設定
     const blob = await response.blob()
-    const video = document.querySelector('video')
-    if (video) {
-      video.src = URL.createObjectURL(blob)
-      video.controls = true
-    }
+    videoObjectURL.value = URL.createObjectURL(blob)
   }
 }
 
-onUnmounted(() => {
-  const video = document.querySelector('video')
-  if (video) {
-    video.pause()
-    video.remove()
-    URL.revokeObjectURL(video.src)
-  }
-})
+const deleteVideo = async () => {
+  // キャッシュストレージの名前を指定してキャッシュインスタンスを生成
+  const cache = await caches.open('video-cache')
+  // キャッシュストレージからリソースを削除
+  return cache.delete(videoURL)
+}
 
+onUnmounted(() => {
+  // loadVideoで生成したObjectURLを解放
+  URL.revokeObjectURL(videoObjectURL.value)
+})
 </script>
 
 <style>
